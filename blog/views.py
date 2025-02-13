659c96bd39a030efd65bf82ef2412d819befdc16
Erysnell <erysnell09@gmail.com>
2025-02-12 22:18:26 -0500

    adding the functionality that only authenticated users and authors of the post can delete and remove it.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -14,18 +15,33 @@ class BlogDetailView(DetailView): # new
  model = Post
  template_name = "post_detail.html"
 
-class BlogCreateView(CreateView): # new
+class BlogCreateView(LoginRequiredMixin,CreateView): 
     model = Post
     template_name = "post_new.html"
-    fields = ["title", "author", "body"]
+    fields = ["title", "body"]
+    login_url = 'login' 
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) # Redirige a esta URL si el usuario no está autenticado
 
-class BlogUpdateView(UpdateView): # new
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # new
  model = Post
  template_name = "post_edit.html"
  fields = ["title", "body"]
+ login_url = 'login'
 
-class BlogDeleteView(DeleteView): # new
+ def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class BlogDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView): # new
  model = Post
  template_name = "post_delete.html"
  success_url = reverse_lazy("home")
- 
+ login_url = 'login'
+
+ def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
